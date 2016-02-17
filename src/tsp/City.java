b902f56bdee3e4172cd64272034a3c3259d7fe8c
batman1292 <batman12922@gmail.com>
2016-02-17 22:49:16 +0700

    add follow web

---
+++ src/tsp/City.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tsp;
+
+/**
+ *
+ * @author 2IDR-PC
+ */
+public class City {
+    double x;
+    double y;
+    
+    public City(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX(){
+        return this.x;
+    }
+    
+    public double getY(){
+        return this.y;
+    }
+    
+    public double distanceTo(City city){
+        double xDistance = Math.abs(getX() - city.getX());
+        double yDistance = Math.abs(getY() - city.getY());
+        double distance = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance) );
+        
+        return distance;
+    }
+    
+    @Override
+    public String toString(){
+        return getX()+", "+getY();
+    }
+}
