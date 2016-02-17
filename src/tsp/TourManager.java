650c7d1f40143fc6aa907e8ccaa04844802baafc
batman1292 <batman12922@gmail.com>
2016-02-17 22:54:34 +0700

    full add

--- src/tsp/TourManager.java
+++ src/tsp/TourManager.java
@@ -5,10 +5,22 @@
  */
 package tsp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 2IDR-PC
  */
 public class TourManager {
+    
+    private static ArrayList destinationCities = new ArrayList<City>();
+   
+    public static City getCity(int index){
+        return (City)destinationCities.get(index);
+    }
+
+    public static int numberOfCities(){
+        return destinationCities.size();
+    }
     
 }
